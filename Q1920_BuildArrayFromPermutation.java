2190eb61b02e2944193be249e2d8616391131fa7
wintongee <wgee01@calpoly.edu>
2022-09-30 09:13:28 -0700

    Update

---
+++ Q1920_BuildArrayFromPermutation.java
@@ -0,0 +1,10 @@
+public class Q1920_BuildArrayFromPermutation {
+
+    public int[] buildArray(int[] nums) {
+        int arr[] = new int[nums.length];
+        for (int i = 0; i < nums.length; i++)
+            arr[i] = nums[nums[i]];
+        return arr;
+    }
+
+}
