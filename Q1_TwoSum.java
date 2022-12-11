cb8213fa188cf00fbcb91445cb021e4bc617fad4
wintongee <wgee01@calpoly.edu>
2022-12-11 15:11:31 -0800

    Update

---
+++ Q1_TwoSum.java
@@ -0,0 +1,20 @@
+public class Q1_TwoSum {
+
+    // Brute force
+    public int[] twoSum(int[] nums, int target) {
+        int[] res = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (i == j)
+                    continue;
+                if (nums[i] + nums[j] == target) {
+                    res[0] = i;
+                    res[1] = j;
+                    return res;
+                }
+            }
+        }
+        return res;
+    }
+
+}
